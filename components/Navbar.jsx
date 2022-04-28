ffd212c24da014e8a0a5938df0c47a7e12c1e477
0cddo <0.c.dddo@gmail.com>
2022-04-28 17:55:10 +0900

    creating navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Navbar.module.css';
+
+export const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
+    </div>
+  );
+};

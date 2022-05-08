a33f4a02be55f978ecbff97cc4281609aa527b0d
0cddo <0.c.dddo@gmail.com>
2022-05-08 15:56:08 +0900

    🗃️ Add Product model schema

---
+++ models/Product.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const ProductSchema = new mongoose.Schema(
+  {
+    title: {
+      type: string,
+      required: true,
+      maxlength: 60,
+    },
+    desc: {
+      type: string,
+      required: true,
+      maxlength: 200,
+    },
+    img: {
+      type: string,
+      required: true,
+    },
+    prices: {
+      type: [Number],
+      required: true,
+    },
+    extraOptions: {
+      type: [
+        {
+          text: { type: string, required: true },
+          price: { type: Number, required: true },
+        },
+      ],
+    },
+  },
+  { timestamps: true }
+);
+
+// mongo database에 Product 모델이 있을 경우는 새로 만들지 않고 기존의 모델 사용하겠다
+export default mongoose.models.Prodct ||
+  mongoose.model('Product', ProductSchema);

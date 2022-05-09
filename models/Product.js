f45a62d9eb679e23a032dc6b43231ad00cf6baac
0cddo <0.c.dddo@gmail.com>
2022-05-09 17:00:30 +0900

    ✏️ 오타 수정 ,,,ㅎㅎ

--- models/Product.js
+++ models/Product.js
@@ -33,5 +33,5 @@ const ProductSchema = new mongoose.Schema(
 );
 
 // mongo database에 Product 모델이 있을 경우는 새로 만들지 않고 기존의 모델 사용하겠다
-export default mongoose.models.Prodct ||
+export default mongoose.models.Product ||
   mongoose.model('Product', ProductSchema);

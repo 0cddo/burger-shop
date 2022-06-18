1b793945c0d374018589be2c3c023e471a52dc30
0cddo <0.c.dddo@gmail.com>
2022-06-18 22:17:54 +0900

    ✨ admin - 주문 내역 상태 업데이트 기능 생성

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -5,6 +5,9 @@ import { useState } from 'react';
 
 const Index = ({ products, orders }) => {
   const [pizzaList, setPizzaList] = useState(products);
+  const [orderList, setOrderList] = useState(orders);
+  // 주문 상태 관리
+  const status = ['preparing', 'cooking', 'delivered'];
 
   const handleDelete = async (id) => {
     try {
@@ -17,6 +20,24 @@ const Index = ({ products, orders }) => {
       console.log(err);
     }
   };
+
+  const handleStatus = async (id) => {
+    const item = orderList.filter((order) => order._id === id)[0];
+    const currentStatus = item.status;
+
+    try {
+      const res = await axios.put(`http://localhost:3000/api/orders/${id}`, {
+        status: currentStatus + 1,
+      });
+      setOrderList([
+        res.data,
+        ...orderList.filter((order) => order._id !== id),
+      ]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.item}>
@@ -74,17 +95,19 @@ const Index = ({ products, orders }) => {
             </tr>
           </tbody>
           <tbody>
-            {orders.map((order) => (
+            {orderList.map((order) => (
               <tr className={styles.item} key={order._id}>
                 <td>{order._id.slice(0, 5)}...</td>
                 <td>{order.customer}</td>
                 <td>${order.total}</td>
                 <td>
-                  {order.method === 0 ? <span>Cash</span> : <span>Paypal</span>}
+                  {order.method === 0 ? <span>Cash</span> : <span>Paid</span>}
                 </td>
-                <td>preparing</td>
+                <td>{status[order.status]}</td>
                 <td>
-                  <button>Next Stage</button>
+                  <button onClick={() => handleStatus(order._id)}>
+                    Next Stage
+                  </button>
                 </td>
               </tr>
             ))}

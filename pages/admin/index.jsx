6ec5fddd947914a4b90eab313aaee6e0893521f5
0cddo <0.c.dddo@gmail.com>
2022-06-18 16:41:05 +0900

    ✨ admin 페이지 생성 및 데이터 연동

---
+++ pages/admin/index.jsx
@@ -0,0 +1,91 @@
+import axios from 'axios';
+import Image from 'next/image';
+import styles from '../../styles/Admin.module.css';
+
+const Index = ({ products, orders }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Products</h1>
+        <table className={styles.table}>
+          <tbody>
+            <tr className={styles.item}>
+              <th>Image</th>
+              <th>Id</th>
+              <th>Title</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </tbody>
+          <tbody>
+            {products.map((product) => (
+              <tr className={styles.item} key={product._id}>
+                <td>
+                  <Image
+                    src={product.img}
+                    width={50}
+                    height={50}
+                    objectFit="cover"
+                    alt="pizza"
+                  />
+                </td>
+                <td>{product._id.slice(0, 5)}...</td>
+                <td>{product.title}</td>
+                <td>${product.prices[0]}</td>
+                <td>
+                  <button className={styles.button}>Edit</button>
+                  <button className={styles.button}>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Orders</h1>
+        <table className={styles.table}>
+          <tbody>
+            <tr className={styles.item}>
+              <th>Id</th>
+              <th>Customer</th>
+              <th>Total</th>
+              <th>Payment</th>
+              <th>Status</th>
+              <th>Action</th>
+            </tr>
+          </tbody>
+          <tbody>
+            {orders.map((order) => (
+              <tr className={styles.item} key={order._id}>
+                <td>{order._id.slice(0, 5)}...</td>
+                <td>{order.customer}</td>
+                <td>${order.total}</td>
+                <td>
+                  {order.method === 0 ? <span>Cash</span> : <span>Paypal</span>}
+                </td>
+                <td>preparing</td>
+                <td>
+                  <button>Next Stage</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const productRes = await axios.get('http://localhost:3000/api/products');
+  const orderRes = await axios.get('http://localhost:3000/api/orders');
+
+  return {
+    props: {
+      products: productRes.data,
+      orders: orderRes.data,
+    },
+  };
+};
+
+export default Index;
